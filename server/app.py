6a343ca676e0223397201932ba263c7773a8d6e6
itsjohncs <johnsullivan.pem@gmail.com>
2021-09-14 14:36:52 -0700

    Follow advice of SQLAlchemy warning.
    This will apparently be the default value in the future and I'm not
    even sure what this feature does when it's enabled, so might as well
    disable it.

--- server/app.py
+++ server/app.py
@@ -4,6 +4,7 @@ import flask_sqlalchemy
 
 app = flask.Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/toptal-interview-project.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # genereate new key with `secrets.token_bytes(20)`
 app.secret_key = b'\x1dz\xf2\xbd\x91\xff\xf1\xb8\xfc\xef\x85z\xad\x98\x8c\x95^\x9f\xc0\x9b'
