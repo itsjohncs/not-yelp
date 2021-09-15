1ff1a9879af2cf0f82d9fba6a9620112331b7a17
itsjohncs <johnsullivan.pem@gmail.com>
2021-09-14 19:00:34 -0700

    Enable foreign key constraints in SQLite.

--- server/app.py
+++ server/app.py
@@ -1,13 +1,22 @@
 import flask
 import flask_sqlalchemy
-
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
 
 app = flask.Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/toptal-interview-project.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # genereate new key with `secrets.token_bytes(20)`
 app.secret_key = b'\x1dz\xf2\xbd\x91\xff\xf1\xb8\xfc\xef\x85z\xad\x98\x8c\x95^\x9f\xc0\x9b'
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/toptal-interview-project.db"
+
+# Only necessary for SQLite backend (to enforce foreign key contraints)
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
 db = flask_sqlalchemy.SQLAlchemy(app)
 
